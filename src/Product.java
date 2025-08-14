1c322f7cff2aede107fb7a42d50dcdd9d909f901
ravimahto69 <krravi073@gmail.com>
2025-08-14 13:20:07 +0530

    Add new product

--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,8 @@
 public class Product {
     public static void main(String[]args){
-        String Name  = "Cemara";
-        double price = 99999.90;
-        int quantity = 5;
+        String Name  = "Television";
+        double price = 40000.99;
+        int quantity = 6;
         System.out.println("Product name :- "+Name);
         System.out.println("Product price :- "+price);
         System.out.println("Product quantity :- "+quantity);
