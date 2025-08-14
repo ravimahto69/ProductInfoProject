7323cd1927deaf269bc6711156ae469bbb3c26ed
ravimahto69 <krravi073@gmail.com>
2025-08-14 13:16:53 +0530

    Setup a ProductInfo

---
+++ src/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    public static void main(String[]args){
+        String Name  = "Cemara";
+        double price = 99999.90;
+        int quantity = 5;
+        System.out.println("Product name :- "+Name);
+        System.out.println("Product price :- "+price);
+        System.out.println("Product quantity :- "+quantity);
+    }
+}
